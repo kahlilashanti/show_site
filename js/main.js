0cb232ece522d1a301ad2be953b662a0b77ed143
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2017-12-08 20:59:55 -0800

    changed the mouseover area to image

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
   let wrapper = document.getElementById('wrapper');
   let topLayer = wrapper.querySelector('.top');
   let handle = wrapper.querySelector('.handle');
+  let image = document.getElementById('image');
   let skew = 0;
   let delta = 0;
 
   if(wrapper.className.indexOf('skewed') != -1){
     skew = 1000;
   }
-  
-  wrapper.addEventListener('mousemove', function(e){
+
+  image.addEventListener('mousemove', function(e){
     delta = (e.clientX - window.innerWidth / 2) * 0.5;
-  
+
     handle.style.left = e.clientX + delta + 'px';
 
     topLayer.style.width= e.clientX + skew + delta + 'px';
