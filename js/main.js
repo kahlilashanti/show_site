620f283e58e6839f55df893c06243e109630c466
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2018-04-03 14:37:47 -0700

    added modal for Razor

--- js/main.js
+++ js/main.js
@@ -11,11 +11,53 @@ document.addEventListener('DOMContentLoaded', function(){
     skew = 1000;
   }
 
-  wrapper.addEventListener('mousemove', function(e){
+  // wrapper.addEventListener('mousemove', function(e){
+  wrapper.addEventListener('hover', function(e){
     delta = (e.clientX - window.innerWidth / 2) * 0.5;
 
     handle.style.left = e.clientX + delta + 'px';
 
     topLayer.style.width= e.clientX + skew + delta + 'px';
   });
+});
+
+//modal code
+$(document).ready(function(){
+
+  var modal = document.getElementById('razorModal');
+
+
+  var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+
+  document.getElementById("razorSynopsis").addEventListener("click", function(){
+    openModal();
+  })
+
+
+  //listen for close click
+  closeBtn.addEventListener('click', closeModal);
+
+  //listen for outside click
+  window.addEventListener('click', clickOutside);
+
+  //function to open modal
+  function openModal(){
+    modal.style.display = "block";
+  }
+
+  //function to close modal
+  function closeModal(){
+    modal.style.display = "none";
+  }
+
+  //function to close modal on window click
+  function clickOutside(e){
+    if(e.target == modal){
+      modal.style.display = "none";
+    }
+  }
+
+// openModal();
+
 });
